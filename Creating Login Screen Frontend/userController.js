909b5167f339baad84d491d253efe2a48183ac36
veeksha45 <veeksha45@gmail.com>
2025-07-27 19:55:16 +0530

    adding login route

---
+++ Creating Login Screen Frontend/userController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Email and password are required' });
+    }
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    res.status(200).json({ message: 'Login successful', user });
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
